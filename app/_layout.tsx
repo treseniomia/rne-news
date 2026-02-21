03899c21efde73b1da1f8087bb41435f201015c3
treseniomia <treseniomia@gmail.com>
2026-02-21 21:52:30 +0800

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,27 +1,3 @@
-// import { AuthProvider } from "@core/AuthCntxt";
-// import { ThemeProvider } from "@core/ThemeContext";
-// import { Stack } from "expo-router";
-// import { SafeAreaProvider } from "react-native-safe-area-context";
-
-// export default function RootLayout() {
-//   return (
-//     <AuthProvider>
-//       <ThemeProvider>
-//         <SafeAreaProvider>
-//           {/* BOSS NOTE: Tinanggal natin ang explicit definition ng details/[id]
-//             para mawala ang 'extraneous' warning at hindi ito maging modal.
-//           */}
-//           <Stack screenOptions={{ headerShown: false }}>
-//             <Stack.Screen name="index" />
-//             <Stack.Screen name="(auth)" options={{ animation: "fade" }} />
-//             <Stack.Screen name="(tabs)" options={{ animation: "fade" }} />
-//           </Stack>
-//         </SafeAreaProvider>
-//       </ThemeProvider>
-//     </AuthProvider>
-//   );
-// }
-
 import { AuthProvider } from "@core/AuthCntxt";
 import { ThemeProvider } from "@core/ThemeContext";
 import { Stack } from "expo-router";
